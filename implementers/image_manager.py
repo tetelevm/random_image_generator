d7e0b9c19d1e286d95b90d91aa83b26c29c9d803
tetelevm <tetelev99@mail.ru>
2021-12-29 01:25:21 +0300

    Added docstrings into the classes

--- implementers/image_manager.py
+++ implementers/image_manager.py
@@ -6,14 +6,17 @@ Reads arguments, gets the necessary phrases, creates folders and the like.
 
 import re
 from pathlib import Path
+from typing import Iterable
 from argparse import ArgumentParser, Namespace
 
 
 __all__ = ["ImageManager"]
 
 
+# Directory with `randomart.py`
 ROOT_PATH = Path(__file__).absolute().parent.parent.absolute()
 
+# English and Russian, another language can be added if necessary
 ONLY_SYMBOLS = re.compile(r"[^a-zA-Zа-яА-ЯёЁ0-9\s]")
 
 
@@ -36,7 +39,9 @@ class ImageManager:
     def get_args() -> Namespace:
         """
         A method that declares all possible startup arguments and reads
-        them. Available arguments:
+        them.
+
+        Available arguments:
         - `path`
             The path to the default directory. The directory must contain
             the file 'text'/'text.txt' (although `path` can also point
@@ -70,11 +75,19 @@ class ImageManager:
         return parser.parse_args()
 
     @staticmethod
-    def read_file(root_path: str | Path) -> iter:
+    def read_file(root_path: str | Path) -> Iterable[str]:
         """
+        Searches and reads the phrase file.
 
-        :param root_path:
-        :return:
+        Searches for files by the passed path as
+        ['{PATH}/text', '{PATH}/text.txt', '{PATH}'].
+        That is, the path must be either a file or a directory with the
+        `text`/`text.txt` file inside. If the file is not found, it
+        raises FileNotFoundError exception. If file is found, it reads
+        it, splits it into lines and returns all nonempty lines.
+
+        :param root_path: the path to the directory with the file or file
+        :return: phrases from the file
         """
 
         root_path = Path(root_path)
@@ -91,7 +104,19 @@ class ImageManager:
 
         raise FileNotFoundError("no text file found")
 
-    def get_phrases(self) -> iter:
+    def get_phrases(self) -> Iterable[str]:
+        """
+        Returns an iterable object of phrases that should be used to
+        generate images.
+
+        If the `-phrase` argument was passed when the script was started,
+        the generation is based only on it, if not, the phrase file is
+        searched and the phrases are taken from there (see
+        `.read_file()`).
+
+        :return: phrases to generate
+        """
+
         if self.args.phrase:
             return [self.args.phrase]
 
@@ -101,6 +126,16 @@ class ImageManager:
         return self.read_file(ROOT_PATH)
 
     def get_data_dir(self) -> Path:
+        """
+        Attempts to define a directory for the generated images.
+
+        If `target_dir` is specified as an absolute path, it is used. If
+        `target_dir` is relative or not specified (then 'data'), then
+        `path` is used as the parent directory.
+
+        :return: directory path
+        """
+
         target_dir = Path(self.args.target)
         if target_dir.is_absolute():
             return target_dir
@@ -113,14 +148,31 @@ class ImageManager:
         return root_dir / target_dir
 
     def create_data_dir(self):
+        """
+        Creates a directory for the generated images if it is missing.
+        """
+
         try:
             self.data_dir.mkdir()
         except FileExistsError:
             pass
 
-    def create_folder(self, phrase: str) -> Path:  # absolute path
+    def create_folder(self, phrase: str) -> Path:
+        """
+        Creates directories for images.
+
+        Removes from the phrase all the characters that could interfere
+        with the creation of the directory (punctuation marks, special
+        characters, etc.), cuts off long phrases and creates the
+        directory. If one already exists, it appends '_1', '_2', ... .
+        Uses `self.data_dir` as root directory (see `.get_data_dir()`).
+
+        :param phrase: the phrase by which the image is generated
+        :return: path to the created directory
+        """
+
         dirname = ONLY_SYMBOLS.sub("", phrase)[:30]
-        folder_path = Path()  # init for IDE
+        folder_path = Path()  # init for IDE,  will be overwritten
 
         is_created, add_num, num = False, "", 0
         while not is_created:
@@ -132,4 +184,4 @@ class ImageManager:
                 num += 1
                 add_num = "_{}".format(num)
 
-        return folder_path
+        return folder_path  # absolute path
--- operators/base.py
+++ operators/base.py
@@ -16,10 +16,22 @@ __all__ = [
 ]
 
 
-COLOR_TYPE = tuple[float, float, float]
+# Value in the range [-1; 1]
+PIXEL_RANGE = float
+COLOR_TYPE = tuple[PIXEL_RANGE, PIXEL_RANGE, PIXEL_RANGE]
 
 
 class OperatorManager(ABCMeta):
+    """
+    A manager for all operators that generate image colors.
+
+    Metaclass for all operators, but don't use it via
+    `(metaclass=OperatorManager)`, instead inherit it from `Operator`
+    class or its subclasses.
+    Stores a list of all operators to generate, and passes the same
+    instance of random to all objects to keep the generation identical.
+    """
+
     arity: int
     _current_random: Random = Random()
 
@@ -29,10 +41,7 @@ class OperatorManager(ABCMeta):
     def __new__(mcs, clsname, bases, dct):
         cls = super().__new__(mcs, clsname, bases, dct)
 
-        # A confusing and obscure hack for calling the same object of random
-        # from all operators
-        cls.random = classmethod(mcs._random_getter)
-
+        # Adding a new operator to the list of all operators
         if ABC not in bases:
             target_operators = (
                 mcs.operators_dimensional
@@ -43,24 +52,48 @@ class OperatorManager(ABCMeta):
 
         return cls
 
-    @property
-    def _random_getter(cls):
-        return cls.__class__._current_random
+    @classmethod
+    def get_random(mcs) -> Random:
+        """
+        A method for getting a random object to all operators.
+        """
+
+        return mcs._current_random
 
     @classmethod
-    def set_random(cls, random_generator):
-        cls._current_random = random_generator
+    def set_random(mcs, random_generator: Random):
+        """
+        A method for setting a random object to all operators.
+        """
+
+        mcs._current_random = random_generator
 
     @classmethod
-    def get_operators_flat(self) -> list[OperatorManager]:
-        return sorted(self.operators_flat, key=lambda x: x.sort_key)
+    def get_operators_flat(mcs) -> list[OperatorManager]:
+        """
+        Sorted list of operators with `arity = 0`.
+        """
+
+        return sorted(mcs.operators_flat, key=lambda x: x.sort_key)
 
     @classmethod
-    def get_operators_dimensional(self) -> list[OperatorManager]:
-        return sorted(self.operators_dimensional, key=lambda x: x.sort_key)
+    def get_operators_dimensional(mcs) -> list[OperatorManager]:
+        """
+        Sorted list of operators with `arity > 0`.
+        """
+
+        return sorted(mcs.operators_dimensional, key=lambda x: x.sort_key)
 
 
 class Operator(ABC, metaclass=OperatorManager):
+    """
+    A class for inheriting all other operators.
+
+    Only non-abstract subclasses (without ABC in parents) are used in
+    image generation. If the class is non-abstract, it must have a
+    complexity class `arity` (>= 0) and generation method `.eval()`.
+    """
+
     arity: int
     random: Random
 
@@ -68,12 +101,39 @@ class Operator(ABC, metaclass=OperatorManager):
     sort_key: int
 
     @abstractmethod
-    def eval(self, x: float, y: float) -> COLOR_TYPE:
+    def eval(self, x: PIXEL_RANGE, y: PIXEL_RANGE) -> COLOR_TYPE:
+        """
+        Color generation based on the pixel's position relative to the
+        center of the image.
+
+        :param x: position on x
+        :param y: position on y
+        :return: rgb-color
+        """
         pass
+
+    @classmethod
+    @property
+    def random(cls) -> Random:
+        """
+        A common instance of random from a metaclass.
+        """
+
+        return cls.__class__.get_random()
 
 
 def operator_subclass_names(locals_: dict[str, object]) -> list[str]:
+    """
+    Function to get the names of all operators to be generated from the
+    current file.
+
+    :param locals_: `locals()` for the current module (not the built-in
+        function, but the dictionary it returns)
+    :return: list of names
+    """
+
     def is_operator_subclass(cls) -> bool:
+        """ Look only for non-abstract Operator subclasses. """
         return isinstance(cls, OperatorManager) and ABC not in cls.__bases__
 
     return [
